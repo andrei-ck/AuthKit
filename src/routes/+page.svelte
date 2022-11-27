164b9bba324c83c51f670f96c6543a2e9052f6a8
andreideak <48556094+andreideak@users.noreply.github.com>
2022-11-27 23:03:16 +0100

    added under construction message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,23 @@
-<h1>Under construction</h1>
+<div class="underConstruction">
+	<p class="road-sign">🏗️</p>
+	<h1>Under construction</h1>
+</div>
 
 <style>
-    h1 {
-        text-align: center;
-    }
+	.underConstruction {
+		margin-top: 100px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-self: center;
+	}
+
+	h1 {
+		text-align: center;
+	}
+	.road-sign {
+		text-align: center;
+		font-size: 160px;
+		margin: 10px;
+	}
 </style>
---
+++ src/routes/jwt/+page.svelte
@@ -0,0 +1,2 @@
+<h1>JWT Decoder</h1>
+
